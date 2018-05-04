0f5533cece3b229665d6d1a07892e11aed3d2553
YANG <yijincheng@qiaodata.com>
2018-05-04 15:25:53 +0800

    基本完成页面效果控件动画效果部分

--- Kotlin/pagelibrary/src/main/java/com/toy/qiaodata/pagelibrary/PageView.kt
+++ Kotlin/pagelibrary/src/main/java/com/toy/qiaodata/pagelibrary/PageView.kt
@@ -11,7 +11,7 @@ import android.view.MotionEvent.*
 import android.view.View
 
 /**
- * 
+ *
  * Created by YANG on 2018/4/26.
  */
 
@@ -23,6 +23,7 @@ class PageView:View {
     /**-----------------------普通成员变量声明区-----------------------*/
     private lateinit var paint:Paint
     private lateinit var lastBitmap:Bitmap
+    private lateinit var currentBitmap:Bitmap
     private lateinit var nextBitmap:Bitmap
     private var centerX = 0f
     private var centerY = 0f
@@ -51,7 +52,8 @@ class PageView:View {
 
     private fun init() {
         lastBitmap = BitmapFactory.decodeResource(resources, R.mipmap.share_resume_weixin)
-        nextBitmap = BitmapFactory.decodeResource(resources,R.mipmap.share_resume_qq)
+        nextBitmap = BitmapFactory.decodeResource(resources, R.mipmap.share_resume_weixin)
+        currentBitmap = BitmapFactory.decodeResource(resources,R.mipmap.share_resume_qq)
 
         paint = Paint()
 
@@ -76,6 +78,17 @@ class PageView:View {
             }
 
             override fun onAnimationEnd(p0: Animator?) {
+                val temple = currentBitmap
+                currentBitmap = nextBitmap
+                nextBitmap = temple
+                lastBitmap = temple
+
+                slideStatus = SlideStatus.Stay
+                animatorStatus = AnimatorStatus.Stay
+                lastX = 0f
+
+                invalidate()
+
             }
         })
 
@@ -86,6 +99,17 @@ class PageView:View {
             }
 
             override fun onAnimationEnd(p0: Animator?){
+                val temple = currentBitmap
+                currentBitmap = lastBitmap
+                nextBitmap = temple
+                lastBitmap = temple
+
+                slideStatus = SlideStatus.Stay
+                animatorStatus = AnimatorStatus.Stay
+                lastX = 0f
+
+                invalidate()
+
             }
 
             override fun onAnimationCancel(p0: Animator?) {
@@ -105,13 +129,18 @@ class PageView:View {
         camera.setLocation(0f,0f,(-lastBitmap.width / 2).toFloat())
 
         drawAdapter = object :DrawAdapter{
-            override fun drawLastStatyPage(canvas: Canvas) {
+            override fun drawCurrentPage(canvas: Canvas,bitmap: Bitmap) {
                 canvas.save()
-                canvas.clipRect(0f,0f,centerX,height.toFloat())
-                canvas.drawBitmap(lastBitmap,mLeft,mTop,paint)
+                canvas.drawBitmap(bitmap,mLeft,mTop,paint)
                 canvas.restore()
             }
-            override fun drawLastMovePage(canvas: Canvas) {
+            override fun drawLastStatyPage(canvas: Canvas,bitmap: Bitmap) {
+                canvas.save()
+                canvas.clipRect(0f,0f,centerX,height.toFloat())
+                canvas.drawBitmap(bitmap,mLeft,mTop,paint)
+                canvas.restore()
+            }
+            override fun drawLastMovePage(canvas: Canvas,bitmap: Bitmap) {
                 canvas.save()
 
                 camera.save()
@@ -122,16 +151,16 @@ class PageView:View {
                 camera.restore()
 
                 canvas.clipRect(centerX,0f,width.toFloat(),height.toFloat())
-                canvas.drawBitmap(lastBitmap,mLeft,mTop,paint)
+                canvas.drawBitmap(bitmap,mLeft,mTop,paint)
                 canvas.restore()
             }
-            override fun drawNextStatyPage(canvas: Canvas) {
+            override fun drawNextStatyPage(canvas: Canvas,bitmap: Bitmap) {
                 canvas.save()
                 canvas.clipRect(centerX,0f,width.toFloat(),height.toFloat())
-                canvas.drawBitmap(nextBitmap,mLeft,mTop,paint)
+                canvas.drawBitmap(bitmap,mLeft,mTop,paint)
                 canvas.restore()
             }
-            override fun drawNextMovePage(canvas: Canvas) {
+            override fun drawNextMovePage(canvas: Canvas,bitmap: Bitmap) {
                 canvas.save()
 
                 camera.save()
@@ -142,7 +171,7 @@ class PageView:View {
                 camera.restore()
 
                 canvas.clipRect(0f,0f,centerX,height.toFloat())
-                canvas.drawBitmap(nextBitmap,mLeft,mTop,paint)
+                canvas.drawBitmap(bitmap,mLeft,mTop,paint)
                 canvas.restore()
             }
         }
@@ -154,37 +183,134 @@ class PageView:View {
     /**-----------------------重载方法区--------------------------*/
     override fun onDraw(canvas: Canvas) {
         getLayout()
-        drawAdapter.drawLastStatyPage(canvas)
-        drawAdapter.drawNextStatyPage(canvas)
-        if (degree <= 90) {
-            drawAdapter.drawLastMovePage(canvas)
-        }
-        if (degree in 90.0..180.0) {
-            drawAdapter.drawNextMovePage(canvas)
+        if (animatorStatus == AnimatorStatus.Stay) {
+            drawAdapter.drawCurrentPage(canvas,lastBitmap)
+        } else {
+            when(animatorStatus) {
+                AnimatorStatus.PrePlaying -> {
+                    drawAdapter.drawLastStatyPage(canvas,currentBitmap)
+                    drawAdapter.drawNextStatyPage(canvas,lastBitmap)
+                    if (degree <= 90) {
+                        drawAdapter.drawLastMovePage(canvas,currentBitmap)
+                    }
+                    if (degree in 90.0..180.0) {
+                        drawAdapter.drawNextMovePage(canvas,lastBitmap)
+                    }
+                }
+                AnimatorStatus.NextPlaying -> {
+                    drawAdapter.drawLastStatyPage(canvas,nextBitmap)
+                    drawAdapter.drawNextStatyPage(canvas,currentBitmap)
+                    if (degree <= 90) {
+                        drawAdapter.drawLastMovePage(canvas,nextBitmap)
+                    }
+                    if (degree in 90.0..180.0) {
+                        drawAdapter.drawNextMovePage(canvas,currentBitmap)
+                    }
+                }
+            }
         }
     }
 
 
     private var lastX: Float = 0f
-
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (event.action == ACTION_DOWN) {
             lastX = event.x
         }
         if (event.action == ACTION_MOVE) {
-            if((lastX - event.x) > 0) {
-                nextPageValueAnimator.currentPlayTime = nextPageValueAnimator.currentPlayTime + 10
+
+            //1.是否开始翻页
+                //1.1没有开始翻页，向左滑还是向右滑
+                    //1.1.1向左滑，启动翻动到下一页动画
+                    //1.1.2向右滑，启动翻动到上一页动画
+                //1.2开始翻页，是上一页动画还是下一页动画
+                    //1.2.1上一页动画 向左滑还是向右滑
+                        //1.2.1.1 向左滑，上一页动画减进度，动画进度是否为0或者为1
+                            //1.2.1.1.1 动画进度是为0或者为1，翻页状态置为否
+                            //1.2.1.1.2 动画进度不为0或者1，不做操作
+                        //1.2.1.2 向右滑，上一页动画增加进度，动画进度是否为0或者为1
+                            //1.2.1.2.1 动画进度是为0或者为1，翻页状态置为否
+                            //1.2.1.2.2 动画进度不为0或者1，不做操作
+                    //1.2.2 下一页动画，向左滑还是向右滑
+                        //1.2.2.1 向左滑，下一页动画减进度，动画进度是否为0或者为1
+                            //1.2.2.1.1 动画进度是为0或者为1，翻页状态置为否
+                            //1.2.2.1.2 动画进度不为0或者1，不做操作
+                        //1.2.2.2 向右滑，下一页动画增加进度，动画进度是否为0或者为1
+                            //1.2.2.2.1 动画进度是为0或者为1，翻页状态置为否
+                            //1.2.2.2.2 动画进度不为0或者1，不做操作
+
+            if (isSliding()) {
+                when(moveDirection(event.x)) {
+                    SlideDirection.Left -> {
+                        animatorStatus = AnimatorStatus.NextPlaying
+                        nextPageValueAnimator.currentPlayTime = nextPageValueAnimator.currentPlayTime + 10
+                    }
+                    SlideDirection.Right -> {
+                        animatorStatus = AnimatorStatus.PrePlaying
+                        prePageValueAnimator.currentPlayTime = prePageValueAnimator.currentPlayTime + 10
+                    }
+                }
             } else {
-                nextPageValueAnimator.currentPlayTime = nextPageValueAnimator.currentPlayTime - 10
+                slideStatus = SlideStatus.Moving
+                when(animatorStatus) {
+                    AnimatorStatus.PrePlaying -> {
+                        when(moveDirection(event.x)) {
+                            SlideDirection.Left -> {
+                                prePageValueAnimator.currentPlayTime = prePageValueAnimator.currentPlayTime - 10
+                            }
+                            SlideDirection.Right -> {
+                                prePageValueAnimator.currentPlayTime = prePageValueAnimator.currentPlayTime + 10
+                            }
+                        }
+                        if (prePageValueAnimator.currentPlayTime == 1000L || prePageValueAnimator.currentPlayTime == 0L || !prePageValueAnimator.isRunning) {
+                            slideStatus = SlideStatus.Stay
+                            animatorStatus = AnimatorStatus.Stay
+                            lastX = 0f
+                        }
+                    }
+
+                    AnimatorStatus.NextPlaying -> {
+                        when(moveDirection(event.x)) {
+                            SlideDirection.Left -> {
+                                nextPageValueAnimator.currentPlayTime = nextPageValueAnimator.currentPlayTime + 10
+                            }
+                            SlideDirection.Right -> {
+                                nextPageValueAnimator.currentPlayTime = nextPageValueAnimator.currentPlayTime - 10
+                            }
+                        }
+                        if (nextPageValueAnimator.currentPlayTime == 1000L || nextPageValueAnimator.currentPlayTime == 0L ||!nextPageValueAnimator.isRunning) {
+                            slideStatus = SlideStatus.Stay
+                            animatorStatus = AnimatorStatus.Stay
+                            lastX = 0f
+                        }
+                    }
+
+                }
             }
             lastX = event.x
+
         }
         if(event.action == ACTION_UP) {
-            nextPageValueAnimator.start()
+            when(animatorStatus) {
+                AnimatorStatus.PrePlaying -> prePageValueAnimator.start()
+                AnimatorStatus.NextPlaying -> nextPageValueAnimator.start()
+            }
         }
         return true
     }
 
+    private fun moveDirection(movedX:Float): SlideDirection {
+        val value = lastX - movedX
+        return when {
+            value > 0 -> SlideDirection.Left
+            value < 0 -> SlideDirection.Right
+            else -> SlideDirection.Stay
+        }
+    }
+
+    private fun isSliding(): Boolean {
+        return slideStatus != SlideStatus.Stay
+    }
 
 
     /**-----------------------普通方法区-----------------------*/
@@ -199,14 +325,27 @@ class PageView:View {
     }
     /**-----------------------内部接口声明区--------------------------*/
     /**-----------------------内部类声明区---------------------------*/
+    var slideStatus:SlideStatus = SlideStatus.Stay
+    enum class SlideStatus {
+        Moving,Stay
+    }
+    enum class SlideDirection {
+        Left,Right,Stay
+    }
+    var animatorStatus:AnimatorStatus = AnimatorStatus.Stay
+    enum class AnimatorStatus {
+        NextPlaying,PrePlaying,Stay
+    }
 
 
 }
 
 interface DrawAdapter {
-    fun drawLastStatyPage(canvas: Canvas)
-    fun drawLastMovePage(canvas: Canvas)
+    fun drawLastMovePage(canvas: Canvas, bitmap: Bitmap)
+    fun drawLastStatyPage(canvas: Canvas, bitmap: Bitmap)
 
-    fun drawNextStatyPage(canvas: Canvas)
-    fun drawNextMovePage(canvas: Canvas)
+    fun drawNextStatyPage(canvas: Canvas, bitmap: Bitmap)
+    fun drawNextMovePage(canvas: Canvas, bitmap: Bitmap)
+
+    fun drawCurrentPage(canvas: Canvas, bitmap: Bitmap)
 }
